c8340a0492efbefa135f7811af867a1561235af3
smertcanyilmaz <yilmazsmertcan@gmail.com>
2024-02-16 01:35:03 +0300

    add create form validation with zod

---
+++ src/app/jobs/new/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+import React from "react";
+
+type Props = {};
+
+export const metadata: Metadata = {
+  title: "Post a new job",
+};
+
+export default function Page({}: Props) {
+  return <div>page</div>;
+}
