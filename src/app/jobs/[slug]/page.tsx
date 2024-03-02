7cc0b8de72363b886b69a0f525fe3c015255baa0
smertcanyilmaz <yilmazsmertcan@gmail.com>
2024-03-02 17:35:55 +0300

    add ssg for slug

--- src/app/jobs/[slug]/page.tsx
+++ src/app/jobs/[slug]/page.tsx
@@ -3,6 +3,7 @@ import prisma from "@/lib/prisma";
 import { notFound } from "next/navigation";
 import { Metadata } from "next";
 import JobPage from "@/components/JobPage";
+import { Button } from "@/components/ui/button";
 
 type Props = {
   params: { slug: string };
@@ -18,6 +19,15 @@ const getJob = cache(async (slug: string) => {
   return job;
 });
 
+export async function generateStaticParams() {
+  const jobs = await prisma.job.findMany({
+    where: { approved: true },
+    select: { slug: true },
+  });
+
+  return jobs.map(({ slug }) => slug);
+}
+
 export async function generateMetadata({
   params: { slug },
 }: Props): Promise<Metadata> {
@@ -29,9 +39,28 @@ export async function generateMetadata({
 
 export default async function page({ params: { slug } }: Props) {
   const job = await getJob(slug);
+
+  const { applicationEmail, applicationUrl } = job;
+
+  const applicationLink = applicationEmail
+    ? `mailto:${applicationEmail}`
+    : applicationUrl;
+
+  if (!applicationLink) {
+    console.error("Job has no application link or email");
+    notFound();
+  }
+
   return (
     <main className="m-auto my-10 flex max-w-5xl flex-col items-center gap-5 px-3 md:flex-row md:items-start">
       <JobPage job={job} />
+      <aside>
+        <Button asChild>
+          <a href={applicationLink} className="w-40 md:w-fit">
+            Apply now
+          </a>
+        </Button>
+      </aside>
     </main>
   );
 }
